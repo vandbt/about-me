98074521dcc0c7bb7d2455e3479f2519ec04572b
vandbt <sharkspin@gmail.com>
2018-05-24 16:13:11 +0900

    feat(resume): fix scrollspy (#4)

--- src/assets/scripts/resume.js
+++ src/assets/scripts/resume.js
@@ -1,41 +1,44 @@
-(function ($) {
+jQuery(function () {
   "use strict"; // Start of use strict
 
-  // code from https://www.w3schools.com/bootstrap/bootstrap_ref_js_scrollspy.asp
+  (function ($) {
+    // code from https://www.w3schools.com/bootstrap/bootstrap_ref_js_scrollspy.asp
 
-  // Add scrollspy to <body>
-  $('div#page-top').scrollspy({
-    target: "#sideNav",
-    offset: 50
-  });
+    // Add scrollspy to <body>
+    $('body').scrollspy({
+      target: ".navbar",
+      offset: 50
+    });
 
-  // Add smooth scrolling on all links inside the navbar
-  $(".navbar-nav a").on('click', function (event) {
+    // Add smooth scrolling on all links inside the navbar
+    $("#navbarSupportedContent a").on('click', function (event) {
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
 
-      // Prevent default anchor click behavior
-      event.preventDefault();
+        // Prevent default anchor click behavior
+        event.preventDefault();
 
-      // Store hash
-      var hash = this.hash;
+        // Store hash
+        var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body, div#page-top').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function () {
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function () {
 
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
 
-    } // End if
+      } // End if
 
-  });
+    });
 
-  // enable bootstrap tooltip
-  $('[data-toggle="tooltip"]').tooltip();
+    // enable bootstrap tooltip
+    $('[data-toggle="tooltip"]').tooltip();
 
-})(jQuery); // End of use strict
+  })(jQuery);
+
+}); // End of use strict
