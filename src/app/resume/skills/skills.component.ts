c9257f2c0b8062bfe154677e2ed0f76d1154b627
vandbt <sharkspin@gmail.com>
2018-05-24 21:57:15 +0900

    refactor(skills): hard-coded to data objects (#4)

--- src/app/resume/skills/skills.component.ts
+++ src/app/resume/skills/skills.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Skill } from '../../models/skill';
+import { SKILLS } from '../../models/skills-data';
+import { KNOWLEDGES } from '../../models/knowledges-data';
+
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent implements OnInit {
+
+  skills = SKILLS;
+  knowledges = KNOWLEDGES;
 
   constructor() { }
 
