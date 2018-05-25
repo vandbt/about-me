98489201fea075ddaf5948709cb22d4e62c1a88e
vadbt <sharkspin@gmail.com>
2018-05-25 13:39:33 +0900

    refactor(api call): adding in-memory-data and api call over http (#4)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from "@angular/core";
 
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
+import { HttpClientModule } from "@angular/common/http";
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+import { InMemoryDataService } from "./services/in-memory-data.service";
+import { MessageService } from "./services/message.service";
+
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from "./app.component";
 import { DefaultComponent } from "./default";
 import { ResumeModule } from "./resume/resume.module";
-
 
 @NgModule({
   declarations: [
@@ -15,10 +21,19 @@ import { ResumeModule } from "./resume/resume.module";
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     ResumeModule,
-    NgbModule.forRoot()
+    HttpClientModule,
+    NgbModule.forRoot(),
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
-  providers: [],
+  providers: [InMemoryDataService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
