c5c16d02acf4e44be08d9e9bc81e0a3d589b51c3
vandbt <sharkspin@gmail.com>
2018-06-08 22:04:34 +0900

    chore(content): fix typo and skills #4

--- src/app/models/knowledges-data.ts
+++ src/app/models/knowledges-data.ts
@@ -1,9 +1,11 @@
 import { Skill } from './skill';
 
 export const KNOWLEDGES: Skill[] = [
-    { name: '비즈니스 도메인 분석, 아키텍처 설계, 어플리케이션 프레임워크 설계 능력', level: 100 },
-    { name: 'TDD, BDD, OOP, 도메인드리븐디자인, 엔터프라이즈 아키텍처 설계', level: 100 },
-    { name: '소셜네트워크, 웹서비스, e-Commerce, 지식 정보 서비스 기반 지식', level: 100 },
-    { name: 'Agile Development', level: 100 }
+    { name: 'Expert in Front-end web development.', level: 100 },
+    { name: 'Expert in server-side web development stack: C#, ASP.NET MVC, SQL Server', level: 100 },
+    { name: 'Having Strong knowledge of OO design and implementation.', level: 100 },
+    { name: 'Experienced with Entity Framework + LINQ', level: 100 },
+    { name: 'Experienced : WCF, RESTful Web Services, Web Protocols.', level: 100 },
+    { name: 'Experienced with Agile methodology', level: 100 }
 ];
 
--- src/app/models/skills-data.ts
+++ src/app/models/skills-data.ts
@@ -1,10 +1,9 @@
 import { Skill } from './skill';
 
 export const SKILLS: Skill[] = [
-    { name: 'Typescript, Javascript, HTML5, CSS, C#.NET', level: 100 },
-    { name: 'Angular, AngularJS, jQuery, gulp', level: 100 },
-    { name: 'Node, Express, ASP.NET, AWS', level: 100 },
-    { name: 'MongoDB, MS SQL Server , SQL Query, Entity Framework, DB 모델링', level: 100 },
+    { name: 'Typescript, Javascript, HTML5, CSS,Angular, AngularJS, jQuery, gulp', level: 100 },
+    { name: 'Node.js, Express, C#, ASP.NET MVC, AWS', level: 100 },
+    { name: 'MongoDB, MS SQL Server, Entity Framework, LINQ, Database modeling', level: 100 },
     { name: 'Git, Bitbucket, SVN, CI (Travis CI, Jenkins), Docker, Elasticsearch', level: 100 },
     { name: 'CodeBeamer ALM, Jira, Confluence, Redmine', level: 100 }
 ];
